82e573fcc293870671f09f05d0026e1364426bcb
rindy1123 <genryusinbaou@gmail.com>
2025-05-21 18:09:49 +0900

    ddd

--- index.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, describe, it } from "bun:test";
-import { add } from ".";
-describe("add", () => {
-  it("should add two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
