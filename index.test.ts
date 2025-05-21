fc1f5ad1cf13c7e27d1bafed66c898ba6786f0ab
rindy1123 <genryusinbaou@gmail.com>
2025-05-21 15:26:44 +0900

    ccc

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,7 @@
 import { expect, describe, it } from "bun:test";
+import { add } from ".";
 describe("add", () => {
   it("should add two numbers", () => {
-    const add = (a: number, b: number) => a + b;
     expect(add(1, 2)).toBe(3);
   });
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export function add(a: number, b: number, c: number) {
-  return a + b + c;
+export function add(a: number, b: number) {
+  return a + b;
 }
